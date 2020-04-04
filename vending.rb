a810218a2d47a0fe56e8dfafb1a2d6d8a0515437
juichiro <juichiro0805@gmail.com>
2020-04-05 00:33:12 +0900

    buyerクラスの作成

--- vending.rb
+++ vending.rb
@@ -12,17 +12,18 @@ class VM
       puts "番号:#{index} #{drink.name}: #{drink.price}"
       index += 1
     end
+        puts '購入したい商品の番号を入力してください'
   end
-  def sell
-    puts "購入する商品の番号を入力してください"
-    number = gets.chomp.to_i
-    sleep(2)
-    if @drinks[number].nil?
-      puts "無効の番号です"
-    else
-      puts "#{@drinks[number].name}が出てきた"
-    end
-  end
+  # def sell
+  #   puts "購入する商品の番号を入力してください"
+  #   number = gets.chomp.to_i
+  #   sleep(2)
+  #   if @drinks[number].nil?
+  #     puts "無効の番号です"
+  #   else
+  #     puts "#{@drinks[number].name}が出てきた"
+  #   end
+  # end
 end
 class Drink
   attr_accessor :name, :price
@@ -32,7 +33,30 @@ class Drink
   end
 end
 
+class Buyer
+  attr_accessor :name, :money
+  def initialize(name,money)
+    @name = name
+    @money = money
+  end
+  def earn(money)
+    @money += money
+  end
+
+  def buy(vm)
+    number = gets.chomp.to_i
+    if vm.drinks[number].nil?
+      puts "その番号は無効です"
+    elsif self.money >= vm.drinks[number].price
+      puts "ゴクゴク、#{vm.drinks[number].name}は美味しいなあ"
+    else
+      puts "お金が足りない。。。"
+    end
+  end
+end
+
 vm1 = VM.new
 vm1.add(Drink.new('coke', 150))
+vm1.add(Drink.new('soda',300))
 vm1.show
-vm1.sell
+Buyer.new('ひろし',800).buy(vm1)
